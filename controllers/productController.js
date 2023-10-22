299858f7e0c216bc12d97ebdb2547ebf2f03052d
NadiaAkter24 <nadia780akter@gmail.com>
2023-10-21 23:08:13 -0400

    life

---
+++ controllers/productController.js
@@ -0,0 +1,55 @@
+const Product = require('../models/product');
+
+// Get all products
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Get a single product
+exports.getProductById = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Add a new product
+exports.addProduct = async (req, res) => {
+    const newProduct = new Product(req.body);
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(201).json(savedProduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Update a product
+exports.updateProduct = async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedProduct) return res.status(404).json({ message: 'Product not found' });
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Delete a product
+exports.deleteProduct = async (req, res) => {
+    try {
+        const deletedProduct = await Product.findByIdAndRemove(req.params.id);
+        if (!deletedProduct) return res.status(404).json({ message: 'Product not found' });
+        res.status(200).json({ message: 'Product deleted' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const productController = require('./controllers/productController');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const DB_URI = 'mongodb+srv://upatel:Z3BmV2ofUb7LbJw9@cluster0.n0akqrt.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch(error => console.error('Could not connect to MongoDB', error));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to DressStore application.' });
+});
+
+// Products Routes
+app.get('/api/products', productController.getAllProducts);
+app.get('/api/products/:id', productController.getProductById);
+app.post('/api/products', productController.addProduct);
+app.put('/api/products/:id', productController.updateProduct);
+app.delete('/api/products/:id', productController.deleteProduct);
+
+// To search by product name
+app.get('/api/products?name=:kw', async (req, res) => {
+    try {
+        const products = await Product.find({ name: { $regex: req.params.kw, $options: 'i' } });
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
